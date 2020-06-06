f995f671f837711fa3e6c15c58d053516bbd839b
Gnlow <entrydark@gmail.com>
2020-06-06 23:27:33 +0900

    유형별 비율 추가

--- analyze.ts
+++ analyze.ts
@@ -2,4 +2,21 @@ import _ from "https://deno.land/x/deno_lodash/mod.ts";
 const { groupBy, mapValue } = _
 import nicknames from "./nicknames.js";
 
-console.log(Object.values( mapValue(groupBy(nicknames, (x: string) => x.length), (y: []) => y.length) ).join("\n") )
+const tester = (x: string) => 
+    [
+        "String + Number",
+        "Number + String",
+        "String",
+        "Number",
+        "Other",
+    ][
+        [
+            /^[a-z]+\d+$/,
+            /^\d+[a-z]+$/,
+            /^[a-z]+$/,
+            /^\d+$/,
+            /.+/,
+        ].findIndex(regex => regex.exec(x))
+    ]
+
+console.log([...Object.entries( mapValue(groupBy(nicknames, tester), (y: []) => y.length) )].map(x => x.join(",")).join("\n") )
